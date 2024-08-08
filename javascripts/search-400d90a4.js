3fc73617352bfc1920065e8a4f413f6a08567e1d
sumikawa <sumikawa@users.noreply.github.com>
2024-08-08 12:44:05 +0000

    Middleman build

---
+++ javascripts/search-400d90a4.js
@@ -0,0 +1,10 @@
+(function() {
+  docsearch({
+    appId: '0B5BZHRPI9',
+    apiKey: 'cad927f6eb5fcbe955b896b1df679d49',
+    indexName: 'donto',
+    inputSelector: '#search',
+    debug: false
+  });
+
+}).call(this);
