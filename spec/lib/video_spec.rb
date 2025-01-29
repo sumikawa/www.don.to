e2d65667c3efc4d617563393b21bb4e02a07810f
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-01-29 14:19:56 +0900

    hdtr

--- spec/lib/video_spec.rb
+++ spec/lib/video_spec.rb
@@ -19,6 +19,21 @@ sample1 = <<~EOS
         vendor_id       : [0][0][0][0]
 EOS
 
+sample2 = <<~EOS
+  Duration: 00:00:10.27, start: 0.000000, bitrate: 14589 kb/s
+  Stream #0:0[0x1](und): Video: hevc (Main 10) (hvc1 / 0x31637668), yuv420p10le(tv, bt2020nc/bt2020/arib-std-b67), 1920x1080, 14342 kb/s, 30 fps, 29.97 tbr, 600 tbn (default)
+      Metadata:
+        creation_time   : 2024-10-20T04:28:38.000000Z
+        handler_name    : Core Media Video
+        vendor_id       : [0][0][0][0]
+        encoder         : HEVC
+      Side data:
+        DOVI configuration record: version: 1.0, profile: 8, level: 4, rpu flag: 1, el flag: 0, bl flag: 1, compatibility id: 4, compression: 0
+        displaymatrix: rotation of -90.00 degrees
+        Ambient Viewing Environment, ambient_illuminance=314.000000, ambient_light_x=0.312700, ambient_light_y=0.329000
+  Stream #0:1[0x2](und): Audio: aac (LC) (mp4a / 0x6134706D), 44100 Hz, stereo, fltp, 182 kb/s (default)
+EOS
+
 RSpec.describe do
   describe 'detect' do
     it 'return hdvideo' do
@@ -29,6 +44,17 @@ RSpec.describe do
                              pixel: '480x270',
                              prefix: 'hd',
                              rotate: 0,
+                           })
+    end
+
+    it 'return hdtrvideo' do
+      result = Video.detect(sample2)
+      expect(result).to eq({
+                             aspect: '9:16',
+                             extops: '',
+                             pixel: '270x480',
+                             prefix: 'hdtr',
+                             rotate: -90,
                            })
     end
   end
