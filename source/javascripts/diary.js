fa0935326b7efd1feab50e248939a200be2c63f7
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-03-19 11:32:37 +0900

    update

--- source/javascripts/diary.js
+++ source/javascripts/diary.js
@@ -1,15 +1,15 @@
 if (RegExp(' AppleWebKit.*Mobile|Opera Mini|Mozilla.*Android').test(navigator.userAgent)) {
-  // 何もしない
+  // Do nothing
 } else {
   document.addEventListener('DOMContentLoaded', function () {
     var addressElements = document.querySelectorAll('pre.address');
     addressElements.forEach(function (element) {
-      var replaced = element.innerHTML.replace(/(.*\n)?([äöüÄÖÜß\w ,.-]+|.*市[^\n]*|.*町[^\n]*|.*県[^\n]*|.*区[^\n]*|.*号[^\n]*)(\n.*)?/, function(match, p1, p2, p3) {
-        return p1 + '<a href="http://maps.google.co.jp/?q=' + p2 + '">' + p2 + '</a>' + p3;
-      });
-
-      var query = RegExp.$2.replace(/\s/g, ''); // スペースを削除
-      element.innerHTML = replaced;
+      var re = /(.*\n)([äöüÄÖÜß\w ,.-]+|.*市[^\n]*|.*町[^\n]*|.*県[^\n]*|.*区[^\n]*|.*号[^\n]*)(\n.*)?/;
+      var matched = element.innerHTML.match(re);
+      if (matched != null && matched[2] != undefined) {
+        element.innerHTML = matched[1] + '<a href="http://maps.google.co.jp/?q=' + matched[2] + '">'
+          + matched[2] + '</a>' + matched[3];
+      }
     });
   });
 }
