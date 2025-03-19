af07623b944b55cc550d08468f0291472729cb8e
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-03-19 18:07:58 +0900

    update

--- source/javascripts/diary.js
+++ source/javascripts/diary.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
       // Get the computed heights
       mainBlockHeight = mainBlock.offsetHeight;
       eachindexBlockHeight = eachindexBlock.offsetHeight;
-      screenHeight = screen.availHeight - 20;
+      screenHeight = screen.availHeight - 30;
 
       // Set both blocks to the height of the taller one
       maxHeight = Math.max(mainBlockHeight, eachindexBlockHeight, screenHeight);
