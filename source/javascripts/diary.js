215a8da920fe2cc3745e02a5f333d93d9f7a5608
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-06-30 17:53:56 +0900

    update

--- source/javascripts/diary.js
+++ source/javascripts/diary.js
@@ -4,7 +4,7 @@ if (RegExp(' AppleWebKit.*Mobile|Opera Mini|Mozilla.*Android').test(navigator.us
   document.addEventListener('DOMContentLoaded', function () {
     var addressElements = document.querySelectorAll('pre.address');
     addressElements.forEach(function (element) {
-      var re = /(.*\n)([\u0041-\u005A\u0061-\u007A\u00C0-\u00FF\u0102\u0103\u0110\u0111\u1EA0-\u1EF9ũơäöüÄÖÜß\w ,.-]+|.*市[^\n]*|.*町[^\n]*|.*県[^\n]*|.*区[^\n]*|.*号[^\n]*)(\n.*)?/;
+      var re = /(.*\n)([\u0041-\u005A\u0061-\u007A\u00C0-\u00FF\u0102\u0103\u0110\u0111\u1EA0-\u1EF9ĩưũơäöüÄÖÜß\w ,./+-]+|.*市[^\n]*|.*町[^\n]*|.*県[^\n]*|.*区[^\n]*|.*号[^\n]*)(\n.*)?/;
       var matched = element.innerHTML.match(re);
       if (matched != null && matched[2] != undefined) {
         element.innerHTML = matched[1] + '<a href="http://maps.google.co.jp/?q=' + matched[2] + '">'
