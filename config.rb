b2462ec3beba76f499ca9206eb869dc5144ed5c9
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-07-05 09:13:27 +0900

    Style/For

--- config.rb
+++ config.rb
@@ -32,7 +32,7 @@ set :markdown, fenced_code_blocks: true,
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
-for y in data.year.thisyear.downto(1995) do
+data.year.thisyear.downto(1995).each do |y|
   proxy "/diary/#{y}.html", '/diary/yearly.html', locals: { year: y }, ignore: true
 end
 
