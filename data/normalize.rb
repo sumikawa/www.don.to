9831b050f2ac52288b228145a787981abbf6ac8b
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-07-05 09:03:32 +0900

    Style/BlockDelimiters

--- data/normalize.rb
+++ data/normalize.rb
@@ -4,18 +4,18 @@ require 'pp'
 
 yaml = YAML.load_file('daylog.yml')
 
-yaml.sort_by! { |i|
+yaml.sort_by! do |i|
   day = ''
   if i.is_a? Hash
-    i.each_key { |k|
+    i.each_key do |k|
       next if k == 'comment'
 
       day = k
-    }
+    end
   else
     day = i
   end
   day
-}
+end
 
 YAML.dump(yaml.reverse, File.open('daylog2.yml', 'w'), { line_width: 1000, header: false })
