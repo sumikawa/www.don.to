6ae924db70432a126a9d74fd117ec37c879b356a
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-07-31 14:27:20 +0900

    Fix Naming/HeredocDelimiterNaming

--- spec/lib/video_spec.rb
+++ spec/lib/video_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require_relative '../../lib/video'
 
-sample1 = <<~EOS
+sample1 = <<~FFMPEG
   Duration: 00:00:28.24, start: 0.000000, bitrate: 9656 kb/s
   Stream #0:0[0x1](und): Video: hevc (Main 10) (hvc1 / 0x31637668), yuv420p10le(tv, bt2020nc/bt2020/arib-std-b67), 1920x1080, 9400 kb/s, 29.99 fps, 30 tbr, 600 tbn (default)
       Metadata:
@@ -17,9 +17,9 @@ sample1 = <<~EOS
         creation_time   : 2024-11-03T05:35:07.000000Z
         handler_name    : Core Media Audio
         vendor_id       : [0][0][0][0]
-EOS
+FFMPEG
 
-sample2 = <<~EOS
+sample2 = <<~FFMPEG
   Duration: 00:00:10.27, start: 0.000000, bitrate: 14589 kb/s
   Stream #0:0[0x1](und): Video: hevc (Main 10) (hvc1 / 0x31637668), yuv420p10le(tv, bt2020nc/bt2020/arib-std-b67), 1920x1080, 14342 kb/s, 30 fps, 29.97 tbr, 600 tbn (default)
       Metadata:
@@ -32,7 +32,7 @@ sample2 = <<~EOS
         displaymatrix: rotation of -90.00 degrees
         Ambient Viewing Environment, ambient_illuminance=314.000000, ambient_light_x=0.312700, ambient_light_y=0.329000
   Stream #0:1[0x2](und): Audio: aac (LC) (mp4a / 0x6134706D), 44100 Hz, stereo, fltp, 182 kb/s (default)
-EOS
+FFMPEG
 
 RSpec.describe do
   describe 'detect' do
