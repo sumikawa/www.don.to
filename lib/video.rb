b35f434d3a85d648a2472097edcc9b3d109c20f7
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-08-03 10:22:39 +0900

    update

--- lib/video.rb
+++ lib/video.rb
@@ -15,6 +15,9 @@ class Video
         if (md = line.match(/(rotate\s+:|rotation\s+of)\s+([-\d]+)/))
           rotate = md[2].to_i
         end
+      end
+
+      lines.each do |line| # rubocop:disable Style/CombinableLoops
         next unless line =~ /Stream .*Video/
 
         case line
