2a4f34903b80f970890cd5d390250a55d148f70a
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-08-20 23:18:18 +0900

    update

--- lib/video.rb
+++ lib/video.rb
@@ -46,7 +46,9 @@ class Video
     end
 
     def cmd_opts(opts:, acodec:, vcodec:)
-      "#{opts[:rotate_opt]} -g 120 -vcodec libx264 -s #{opts[:pixel]} -bt 1536k -acodec #{acodec} -ar 32000 -ac 1 -ab 48k -movflags faststart #{opts[:ext_opt]} -f #{vcodec}"
+      video_opt = "-g 120 -vcodec libx264 -s #{opts[:pixel]} -bt 1536k -movflags faststart -f #{vcodec}"
+      audio_opt = "-acodec #{acodec} -ar 32000 -ac 1 -ab 48k"
+      "#{opts[:rotate_opt]} #{video_opt} #{audio_opt} #{opts[:ext_opt]}"
     end
 
     def probe(src)
