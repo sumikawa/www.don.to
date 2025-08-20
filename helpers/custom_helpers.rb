7268f6e434af7043f42d8cede4a342808c6639a2
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-08-20 23:08:20 +0900

    update

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -26,101 +26,15 @@ module CustomHelpers
   end
 
   def gen_index(dirpath)
-    body = ['---', 'draft: true', 'title: ', '---']
-    newbody = []
     now = Time.now
-    Dir.glob(File.expand_path("#{data.site.imagerootdir}/diary/#{dirpath}/*")).sort.each do |f|
-      file = File.basename(f).downcase
-      ext = File.extname(file)
-      base = File.basename(file, ext)
-      ex = begin
-        MiniExiftool.new(f)
-      rescue StandardError
-        {}
-      end
-      t = now
-      text = ''
+    files_data = Dir.glob(File.expand_path("#{data.site.imagerootdir}/diary/#{dirpath}/*")).sort.map do |f|
+      _process_file(f, dirpath, now)
+    end.compact
 
-      case ext.downcase
-      when '.jpg', '.heic'
-        text = "<%= image \"#{base}\" %>"
-        t = ex['SubSecDateTimeOriginal'].to_s.sub(/\.\d\d\d/, '')
-        t = ex['DateTimeOriginal'] || now if t == ''
+    sorted_files_data = files_data.sort_by { |data| data[0] }
 
-        if localhost?
-          FileUtils.mkdir_p(File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}"))
-          data.site.heights.each do |height|
-            filepath = File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}/#{base}.#{data.site.thumbext}")
-            next if File.exist?(filepath)
-
-            image = Magick::Image.read(f).first
-            image = image.resize_to_fit(0, height)
-            image.write(filepath)
-          end
-        end
-      when '.png', '.pdf'
-        text = "<%= image \"#{base}\", ext: 'png' %>" if ext.downcase == 'png'
-
-        if localhost?
-          FileUtils.mkdir_p(File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}"))
-          FileUtils.copy(f, File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}/#{file}"))
-        end
-      when '.mov', '.mp4', '.mts', '.mpg', '.avi'
-        opts = nil
-
-        if localhost?
-          # Generate video for external
-          opts = Video.probe(f)
-
-          video_dir = File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}")
-          video_file = "#{opts[:prefix]}#{base}.#{data.site.videoext}"
-          FileUtils.mkdir_p(video_dir)
-
-          thumb_dir = File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}/")
-          thumb_file = "#{opts[:prefix]}#{base}.#{data.site.thumbext}"
-
-          unless File.exist?("#{video_dir}/#{video_file}")
-            Video.convert(src: f, dst_dir: video_dir, dst_file: video_file,
-                          acodec: data.site.acodec, vcodec: data.site.videoext,
-                          opts: opts)
-          end
-          unless File.exist?("#{thumb_dir}/#{thumb_file}")
-            Video.poster(src: "#{video_dir}/#{video_file}", dst_dir: thumb_dir, dst_file: thumb_file,
-                         height: data.site.thumbheight)
-          end
-        end
-
-        if ext.downcase == '.avi'
-          text = "<%= movie \"#{base}\" %>"
-        else
-          text = if opts
-                   "<%= movie \"#{opts[:prefix]}#{base}\" %>"
-                 else
-                   # recoginized as HD video if it's already exist
-                   "<%= movie \"hd#{base}\" %>"
-                 end
-          t = ex['CreationDate'] || ex['FileModifyDate'] || now
-        end
-      when '.m4a'
-        text = "<%= audio \"#{base}\" %>"
-
-        if localhost?
-          FileUtils.mkdir_p(File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}"))
-          FileUtils.copy(f, File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}/#{file}"))
-        end
-      when '.aae'
-        File.delete(f)
-      else
-        text = "debugging: \"#{file}\""
-      end
-      #      text = "#{t} #{text}<br />"
-      newbody << [t, text]
-    end
-
-    newbody.sort! { |a, b| a[0] <=> b[0] }
-    newbody.each do |n|
-      body << n[1]
-    end
+    body = ['---', 'draft: true', 'title: ', '---']
+    body.concat(sorted_files_data.map { |data| data[1] })
 
     File.open("source/diary/#{dirpath}.html.md.erb", 'w') do |f|
       f.puts(body.join("\n"))
@@ -174,5 +88,113 @@ module CustomHelpers
            end
     dir = filename.sub(/^source/, '').sub('.md.erb', '')
     "<dt>#{date}: " + link_to(title, dir, target) + "#{secret}</dt>"
+  end
+
+  private
+
+  def _process_file(file_path, dirpath, now)
+    file_name = File.basename(file_path).downcase
+    ext = File.extname(file_name)
+    base = File.basename(file_name, ext)
+    file_info = { path: file_path, name: file_name, ext: ext, base: base }
+
+    exif_data = begin
+      MiniExiftool.new(file_path)
+    rescue StandardError
+      {}
+    end
+
+    case ext.downcase
+    when '.jpg', '.heic', '.png', '.pdf'
+      _process_image(file_info, exif_data, dirpath, now)
+    when '.mov', '.mp4', '.mts', '.mpg', '.avi'
+      _process_video(file_info, exif_data, dirpath, now)
+    when '.m4a'
+      _process_audio(file_info, dirpath, now)
+    when '.aae'
+      File.delete(file_info[:path])
+      nil
+    else
+      [now, "debugging: \"#{file_info[:name]}"]
+    end
+  end
+
+  def _process_image(file_info, exif_data, dirpath, now)
+    timestamp = exif_data['SubSecDateTimeOriginal'].to_s.sub(/\.\d{3}/, '')
+    timestamp = exif_data['DateTimeOriginal'] || now if timestamp.empty?
+
+    text = if file_info[:ext].downcase == '.png'
+             "<%= image \"#{file_info[:base]}\", ext: 'png' %>"
+           else
+             "<%= image \"#{file_info[:base]}\" %>"
+           end
+
+    if localhost?
+      cache_dir = _ensure_cache_dir(dirpath)
+      if file_info[:ext].downcase == '.jpg' || file_info[:ext].downcase == '.heic'
+        data.site.heights.each do |height|
+          filepath = File.join(cache_dir, "#{file_info[:base]}.#{data.site.thumbext}")
+          next if File.exist?(filepath)
+
+          image = Magick::Image.read(file_info[:path]).first
+          image.resize_to_fit(0, height).write(filepath)
+        end
+      else # .png, .pdf
+        FileUtils.copy(file_info[:path], File.join(cache_dir, file_info[:name]))
+      end
+    end
+
+    [timestamp, text]
+  end
+
+  def _process_video(file_info, exif_data, dirpath, now)
+    opts = localhost? ? Video.probe(file_info[:path]) : nil
+
+    if localhost?
+      video_dir = _ensure_cache_dir(dirpath)
+      video_file = "#{opts[:prefix]}#{file_info[:base]}.#{data.site.videoext}"
+      video_path = File.join(video_dir, video_file)
+
+      unless File.exist?(video_path)
+        Video.convert(src: file_info[:path], dst_dir: video_dir, dst_file: video_file,
+                      acodec: data.site.acodec, vcodec: data.site.videoext,
+                      opts: opts)
+      end
+
+      thumb_file = "#{opts[:prefix]}#{file_info[:base]}.#{data.site.thumbext}"
+      thumb_path = File.join(video_dir, thumb_file)
+      unless File.exist?(thumb_path)
+        Video.poster(src: video_path, dst_dir: video_dir, dst_file: thumb_file,
+                     height: data.site.thumbheight)
+      end
+    end
+
+    text = if file_info[:ext].downcase == '.avi'
+             "<%= movie \"#{file_info[:base]}\" %>"
+           else
+             prefix = opts ? opts[:prefix] : 'hd' # Assume hd if not local
+             "<%= movie \"#{prefix}#{file_info[:base]}\" %>"
+           end
+
+    timestamp = exif_data['CreationDate'] || exif_data['FileModifyDate'] || now
+
+    [timestamp, text]
+  end
+
+  def _process_audio(file_info, dirpath, now)
+    text = "<%= audio \"#{file_info[:base]}\" %>"
+
+    if localhost?
+      cache_dir = _ensure_cache_dir(dirpath)
+      FileUtils.copy(file_info[:path], File.join(cache_dir, file_info[:name]))
+    end
+
+    [now, text] # Audio files don't have a reliable timestamp in exif
+  end
+
+  def _ensure_cache_dir(dirpath)
+    cache_dir = File.expand_path("#{data.site.cacherootdir}/diary/#{dirpath}")
+    FileUtils.mkdir_p(cache_dir)
+    cache_dir
   end
 end
