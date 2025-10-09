39c458d67ab7e59f564cb36b5f512423ec01811d
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-10-09 22:35:43 +0900

    update

--- source/javascripts/diary.js
+++ source/javascripts/diary.js
@@ -14,12 +14,8 @@ if (RegExp(' AppleWebKit.*Mobile|Opera Mini|Mozilla.*Android').test(navigator.us
   });
 }
 
-docsearch({
-  appId: '0B5BZHRPI9',
-  apiKey: 'cad927f6eb5fcbe955b896b1df679d49',
-  indexName: 'donto',
-  inputSelector: '#search',
-  debug: false
+window.addEventListener('DOMContentLoaded', (event) => {
+  new PagefindUI({ element: "#search", showSubResults: true });
 });
 
 function resizeBlock() {
