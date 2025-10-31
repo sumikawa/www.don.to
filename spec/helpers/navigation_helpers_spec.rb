4236c9ee7d4641402c60eddff610d1bbdab6761a
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-10-31 21:00:59 +0900

    update

---
+++ spec/helpers/navigation_helpers_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+require_relative '../../helpers/navigation_helpers'
+require 'ostruct'
+
+RSpec.describe NavigationHelpers do
+  let(:helper) do
+    class TestHelper
+      include NavigationHelpers
+    end
+    TestHelper.new
+  end
+
+  let(:sitemap) { OpenStruct.new(resources: pages) }
+
+  let(:pages) do
+    [
+      OpenStruct.new(path: 'diary/2023/0101-foo.html', ext: '.html', data: OpenStruct.new(draft: false), url: '/diary/2023/0101-foo.html'),
+      OpenStruct.new(path: 'diary/2023/0102-bar.html', ext: '.html', data: OpenStruct.new(draft: false), url: '/diary/2023/0102-bar.html'),
+      OpenStruct.new(path: 'diary/2023/0103-baz.html', ext: '.html', data: OpenStruct.new(draft: false), url: '/diary/2023/0103-baz.html'),
+      OpenStruct.new(path: 'diary/2023/0104-draft.html', ext: '.html', data: OpenStruct.new(draft: true), url: '/diary/2023/0104-draft.html'),
+      OpenStruct.new(path: 'other/page.html', ext: '.html', data: OpenStruct.new(draft: false), url: '/other/page.html')
+    ]
+  end
+
+  context 'with a diary page in the middle' do
+    let(:current_page) { pages[1] }
+    it 'returns links to both prev and next pages' do
+      nav = helper.diary_nav(current_page, sitemap)
+      expect(nav).to include('<a href="/diary/2023/0101-foo.html">&laquo; prev</a>')
+      expect(nav).to include('<a href="/diary/2023/0103-baz.html">next &raquo;</a>')
+      expect(nav).to include('<span style="margin: 0 1em;">|</span>')
+    end
+  end
+
+  context 'with the first diary page' do
+    let(:current_page) { pages[0] }
+    it 'returns a link to only the next page' do
+      nav = helper.diary_nav(current_page, sitemap)
+      expect(nav).not_to include('&laquo; prev</a>')
+      expect(nav).to include('<a href="/diary/2023/0102-bar.html">next &raquo;</a>')
+      expect(nav).not_to include('<span style="margin: 0 1em;">|</span>')
+    end
+  end
+
+  context 'with the last diary page' do
+    let(:current_page) { pages[2] }
+    it 'returns a link to only the prev page' do
+      nav = helper.diary_nav(current_page, sitemap)
+      expect(nav).to include('<a href="/diary/2023/0102-bar.html">&laquo; prev</a>')
+      expect(nav).not_to include('next &raquo;</a>')
+      expect(nav).not_to include('<span style="margin: 0 1em;">|</span>')
+    end
+  end
+
+  context 'with a non-diary page' do
+    let(:current_page) { pages[4] }
+    it 'returns nil' do
+      nav = helper.diary_nav(current_page, sitemap)
+      expect(nav).to be_nil
+    end
+  end
+
+  context 'with a draft page' do
+    let(:current_page) { pages[3] }
+    it 'returns nil because draft pages are not in the list' do
+      nav = helper.diary_nav(current_page, sitemap)
+      expect(nav).to be_nil
+    end
+  end
+end
