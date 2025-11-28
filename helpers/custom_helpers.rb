09e963c32a6bbddfa67c231905b1ee1f3bf9994f
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-11-28 22:48:04 +0900

    update

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -42,9 +42,7 @@ module CustomHelpers
       <div class="embeded">
         <div class="amazon">
           <div class="amazon-image">#{link_to(img, url)}</div>
-          <div class="amazon-info">
-            <div class="amazon-name">#{link_to(title, url, {class: 'amazon-link'})}</div>
-          </div>
+          <div class="amazon-info">#{link_to(title, url, {class: 'amazon-link'})}</div>
           <div class="amazon-footer"></div>
         </div>
       </div>
