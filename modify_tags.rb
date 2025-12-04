bdaf33c1780285c8098d96952dc809cdb859ebbf
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-12-05 08:28:09 +0900

    update

--- modify_tags.rb
+++ modify_tags.rb
@@ -283,9 +283,9 @@ Dir.glob('source/diary/**/*.md.erb').each do |file|
       end
 
       new_tags = new_tags.sort.uniq
-      if tags != new_tags
+      update_content = content.sub(/^tags:.*$/, "tags: #{new_tags.join(', ')}")
+      if content != update_content
         puts "#{tags.join(', ')} -> #{new_tags.join(', ')}"
-        update_content = content.sub(/^tags:.*$/, "tags: #{new_tags.join(', ')}")
         File.write(file, update_content)
       end
     end
