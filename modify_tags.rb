bfd1ac82c3b1759cdd06963aac55946dc78977ae
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-12-04 20:35:06 +0900

    update

--- modify_tags.rb
+++ modify_tags.rb
@@ -152,6 +152,9 @@ Dir.glob('source/diary/**/*.md.erb').each do |file|
       if new_tags.include?('park') & new_tags.include?('play')
         new_tags.delete('play')
       end
+      if new_tags.include?('minatomirai') & new_tags.include?('yokohama')
+        new_tags.delete('yokohama')
+      end
 
       new_tags = new_tags.sort.uniq
       if tags != new_tags
