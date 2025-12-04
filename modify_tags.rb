a0383783dd826d66f2398a6b975fcacf0d0606bf
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-12-05 07:41:03 +0900

    update

--- modify_tags.rb
+++ modify_tags.rb
@@ -197,8 +197,8 @@ Dir.glob('source/diary/**/*.md.erb').each do |file|
           new_tags.push('playground')
         elsif tag.match?(/^archive$/i)
           new_tags.push('document')
-        elsif tag.match?(/^berlin$/i)
-          new_tags.push('germany')
+        elsif tag.match?(/^biwako$/i)
+          new_tags.push('shiga')
         else
           new_tags.push(tag)
         end
