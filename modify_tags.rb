647b7e472acaa599634e7f8b07cdbae93113ec3f
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-12-05 10:34:29 +0900

    update

--- modify_tags.rb
+++ modify_tags.rb
@@ -209,8 +209,8 @@ Dir.glob('source/diary/**/*.md.erb').each do |file|
           new_tags.push('katsu')
         elsif tag.match?(/^dental$/i)
           new_tags.push('tooth')
-        elsif tag.match?(/^fugu$/i)
-          new_tags.push('seafood')
+        elsif tag.match?(/^fruit$/i)
+          new_tags.push('fruits')
         else
           new_tags.push(tag)
         end
