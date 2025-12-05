4168d6ad0ae00cf370e141754f85429ef6e0f35f
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-12-05 10:33:41 +0900

    update

--- modify_tags.rb
+++ modify_tags.rb
@@ -221,7 +221,7 @@ Dir.glob('source/diary/**/*.md.erb').each do |file|
 
       new_tags = new_tags.map(&:downcase).map(&:strip).delete_if{ |t| t == '' }
 
-      new_tags.delete('hair')
+      new_tags.delete('fantasy')
 
       # puts new_tags
 
