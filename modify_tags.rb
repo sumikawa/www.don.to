16fae301e42ac201813e3b9a9219ccbe4ec7c4bb
Munechika Sumikawa <sumikawa@sumikawa.jp>
2025-12-05 17:09:07 +0900

    update

--- modify_tags.rb
+++ modify_tags.rb
@@ -239,6 +239,12 @@ Dir.glob('source/diary/**/*.md.erb').each do |file|
       new_tags.push('lunch') if content.match?(/^title: 目黒ごはん/) & !new_tags.include?('dinner')
       new_tags.push('lunch') if content.match?(/^title: 東品川ごはん/) & !new_tags.include?('dinner')
 
+      if content.match?(/^title: .*出張/)
+        new_tags.push('businesstrip')
+        new_tags.delete('business') if new_tags.include?('business')
+        new_tags.delete('travel') if new_tags.include?('travel')
+      end
+
       new_tags = new_tags.map(&:downcase).map(&:strip).delete_if{ |t| t == '' }
 
       new_tags.delete('english')
