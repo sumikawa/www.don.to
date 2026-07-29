f6a0a2e483e4cd377852b2c94ff8821905737acc
Munechika Sumikawa <sumikawa@sumikawa.jp>
2026-07-29 11:21:38 +0900

    update

--- helpers/daylog_helpers.rb
+++ helpers/daylog_helpers.rb
@@ -49,8 +49,8 @@ module DaylogHelpers
     item.each do |k, v|
       v = process_amazon(v)
       if k == 'comment'
-        v = v.gsub("\n", "</dd>\n\t<dd>")
-        comment = "\n\t<dd>#{v}</dd>"
+        v = v.gsub("\n", "</dd>\n\t<dd>　")
+        comment = "\n\t<dd>　#{v}</dd>"
       else
         log = "<dt>#{k}: #{v}</dt>"
       end
